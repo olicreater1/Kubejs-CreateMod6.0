a5fec97a9f4e1749998412af73d3bb77239a7120
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2025-04-25 18:49:25 -0800

    Update tuff_server_event.js

--- Create/tuff_server_event.js
+++ Create/tuff_server_event.js
@@ -1,26 +1,24 @@
 //create recipe for tuff from compacting andesite, charcoal, and lava
 ServerEvents.recipes(event => {
-  event.custom({
-    {
+event.custom({
   "type": "create:compacting",
-  "ingredients": [
-    {
-      "item": "minecraft:charcoal"
-    },
-    {
-      "item": "minecraft:andesite"
-    },
-    {
-      "amount": 100,
-      "fluid": "minecraft:lava",
-      "nbt": {}
-    }
-  ],
-  "results": [
-    {
-      "item": "minecraft:tuff"
-    }
-  ]
-    },
+    "ingredients": [
+      {
+        "item": "minecraft:charcoal"
+      },
+      {
+        "item": "minecraft:andesite"
+      },
+      {
+        "amount": 100,
+        "fluid": "minecraft:lava",
+        "nbt": {}
+      }
+    ],
+    "results": [
+      {
+        "item": "minecraft:tuff"
+      },
+    ],
   })
 })
